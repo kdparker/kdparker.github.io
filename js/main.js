931dc5ae361e83d2e1e2ed25dd8d505f1af476f5
Keegan Parker <keegandparker@gmail.com>
2015-06-06 22:57:36 -0400

    Animate hamburger click

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ $().ready(function() {
 	var hamburger = $("#menu");
 
 	hamburger.on('click', function() {
-		$(".navs-container").toggleClass('open');
+		$(".navs-container").slideToggle();
 		$(".hamburger").toggleClass('open');
+	});
+
+	// Hack to make sure everything is fixed on window resize (slideToggle affects html and overwrites the css)
+	$(window).on('resize', function() {
+		if ((!$(".navs-container").is(":visible")) && $(window).width() > 650) {
+			$(".navs-container").toggle();
+		}
+
+		if ($(".navs-container").is(":visible") && $(window).width() <= 650) {
+			$(".hamburger").removeClass('open');
+			$(".navs-container").css('display', "none");
+		}
 	});
 });
