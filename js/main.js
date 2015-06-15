0ac26c25a12e80ede1c36fdfeed14d5baef05eca
Keegan Parker <keegandparker@gmail.com>
2015-06-14 21:59:36 -0400

    Box shadows on nav bar

--- js/main.js
+++ js/main.js
@@ -4,16 +4,19 @@ $().ready(function() {
 	hamburger.on('click', function() {
 		$(".navs-container").slideToggle();
 		$(".hamburger").toggleClass('open');
+		$('.navbar').toggleClass('open');
 	});
 
 	// Hack to make sure everything is fixed on window resize (slideToggle affects html and overwrites the css)
 	$(window).on('resize', function() {
 		if ((!$(".navs-container").is(":visible")) && $(window).width() > 650) {
 			$(".navs-container").toggle();
+			$('.navbar').removeClass('open');
 		}
 
 		if ($(".navs-container").is(":visible") && $(window).width() <= 650) {
 			$(".hamburger").removeClass('open');
+			$('.navbar').removeClass('open');
 			$(".navs-container").css('display', "none");
 		}
 	});
