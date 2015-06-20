7a4ceff061a2337b00abac69668e7c3858329ae0
Keegan Parker <keegandparker@gmail.com>
2015-06-19 23:35:56 -0400

    Mouseover on projects now works

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-$().ready(function() {
-	var hamburger = $("#menu");
+$(document).ready(function () {
+    'use strict';
+    var hamburger = $("#menu");
 
-	hamburger.on('click', function() {
-		$(".navs-container").slideToggle();
-		$(".hamburger").toggleClass('open');
-		$('.navbar').toggleClass('open');
-	});
+    hamburger.on('click', function () {
+        $(".navs-container").slideToggle();
+        $(".hamburger").toggleClass('open');
+        $('.navbar').toggleClass('open');
+    });
 
-	// Hack to make sure everything is fixed on window resize (slideToggle affects html and overwrites the css)
-	$(window).on('resize', function() {
-		if ((!$(".navs-container").is(":visible")) && $(window).width() > 600) {
-			$(".navs-container").toggle();
-			$('.navbar').removeClass('open');
-		}
+    // Hack to make sure everything is fixed on window resize (slideToggle affects html and overwrites the css)
+    $(window).on('resize', function () {
+        if ((!$(".navs-container").is(":visible")) && $(window).width() > 600) {
+            $(".navs-container").toggle();
+            $('.navbar').removeClass('open');
+        }
 
-		if ($(".navs-container").is(":visible") && $(window).width() <= 600) {
-			$(".hamburger").removeClass('open');
-			$('.navbar').removeClass('open');
-			$(".navs-container").css('display', "none");
-		}
-	});
+        if ($(".navs-container").is(":visible") && $(window).width() <= 600) {
+            $(".hamburger").removeClass('open');
+            $('.navbar').removeClass('open');
+            $(".navs-container").css('display', "none");
+        }
+    });
+
+    $(".project-photo").hover(function () {
+        $(this).find('.project-hover').addClass('mouseover');
+    }, function () {
+        $(this).find('.project-hover').removeClass('mouseover');
+    });
 });
