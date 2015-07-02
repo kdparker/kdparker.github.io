cd10cc1d5a808fbec17ddd0cb4e20d4014039806
Keegan Parker <keegandparker@gmail.com>
2015-07-01 22:24:39 -0400

    Small tweaks, added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
         $('.navbar').toggleClass('open');
     });
 
+    function closeHamburgerIfOpen() {
+        if ($(".navs-container").is(":visible") && $(window).width() <= 615) {
+            $(".hamburger").removeClass('open');
+            $('.navbar').removeClass('open');
+            $(".navs-container").css('display', "none");
+        }
+    }
+
     // Hack to make sure everything is fixed on window resize (slideToggle affects html and overwrites the css)
     $(window).on('resize', function () {
         if ((!$(".navs-container").is(":visible")) && $(window).width() > 615) {
@@ -15,16 +23,31 @@ $(document).ready(function () {
             $('.navbar').removeClass('open');
         }
 
-        if ($(".navs-container").is(":visible") && $(window).width() <= 615) {
-            $(".hamburger").removeClass('open');
-            $('.navbar').removeClass('open');
-            $(".navs-container").css('display', "none");
-        }
+        closeHamburgerIfOpen();
     });
 
     $(".project-photo").hover(function () {
         $(this).find('.project-info').addClass('mouseover');
     }, function () {
         $(this).find('.project-info').removeClass('mouseover');
+    });
+
+    $('#articles-link').click(function(){
+        if ($("#articles").length) {
+            closeHamburgerIfOpen();
+            $('html, body').animate({
+                scrollTop: $('#articles').offset().top
+            }, 500);
+        } else {
+            window.location = $(this).attr('href');
+        }
+        return false;
+    });
+
+    $('#projects-link').click(function(){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
     });
 });
